ca5b6a584eb651defdde9ea28d407db5c1ba20a8
Adam Antkowiak <adam.antkowiak93@gmail.com>
2021-03-30 20:28:05 +0200

    Install bootstrap and reactstrap, test button component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import { Button } from 'reactstrap';
 
 function App() {
-  return <div className="App">blabla</div>;
+  return (
+    <div className="App">
+      <Button color="danger">Danger!</Button>
+    </div>
+  );
 }
 
 export default App;
