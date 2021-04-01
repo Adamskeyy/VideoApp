7b3e56d661930327c4abace7e1b690757e32419c
Adam Antkowiak <adam.antkowiak93@gmail.com>
2021-04-01 09:58:18 +0200

    Create basic folder structure for redux store with actions and reducers, setup redux store in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
+// application
 import App from './App';
+// styles
 import 'bootstrap/dist/css/bootstrap.min.css';
+// redux
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+// reducers
+import videoAppReducer from './store/reducers/videoApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  videoApp: videoAppReducer,
+});
+
+const store = createStore(rootReducer);
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
